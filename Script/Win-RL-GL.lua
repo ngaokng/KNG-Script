d094fa17f0beaaa7362217b989eedc1889c849dd
!/KNG <azuna.tool@gmail.com>
2025-07-28 22:56:15 +0200

    Delete Script directory

--- Script/Win-RL-GL.lua
+++
@@ -1,5 +0,0 @@
--- Win | Red Light, Green Light
-local Root = game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
-if Root then
-    Root.CFrame = CFrame.new(-45.040992736816406, 1024.8333740234375, 139.14952087402344)
-end
